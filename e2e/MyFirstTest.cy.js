0a71a9c5574ac35483e98957c97d33cd943927a1
sondinh <sonilu29112003@gmail.com>
2025-06-19 15:15:06 +0700

    none

--- e2e/MyFirstTest.cy.js
+++ e2e/MyFirstTest.cy.js
@@ -1,17 +1,21 @@
-it ("5 Kiem tra Ke sach khi ngung theo doi truyen Test", function(){
+it("6 Hien thi canh bao khi theo doi truyen ma chua dang nhap", () => {
 
-    cy.visit('https://docln.sbs/login')
+  cy.visit("https://docln.sbs");
 
-    cy.get('#name').type('baodinh112903')
-    cy.get('#password').type('29112003')
-    cy.pause()
-    cy.get('.btn-primary').click({force: true})
-    cy.url().should('eq', 'https://docln.sbs/');
-    
-    cy.wait(2000)
-    
-    cy.get(".nav-user_icon").click({ delay: 100 })
-    cy.contains('Kệ sách').click({ delay: 100 })  
-    cy.url().should('include', '/ke-sach')
-    cy.contains('.series-name a', 'Người giấy').should('not.exist');
-})
+  cy.wait(2000)
+  cy.get('#sidenav-icon').click()
+  cy.contains('a.nav-menu_item', 'Sáng tác').click();
+  cy.get('.popular-thumb-item a')
+  .then($items => {
+    const randomIndex = Math.floor(Math.random() * $items.length);
+    cy.wrap($items[randomIndex]).click({ force: true });
+  });
+   //chỉ định truyện
+    // cy.contains('.popular-thumb-item a', 'ten truyen').click({ force: true });
+
+  cy.get('#collect').click();
+
+  cy.on("window:alert", (message) => {
+    expect(message).to.equal("Cần đăng nhập để thực hiện!");
+  }); 
+});
--- e2e/NewKeSach.cy.js
+++ e2e/NewKeSach.cy.js
@@ -139,3 +139,25 @@ it ("5 Kiem tra Ke sach khi ngung theo doi truyen Test", function(){
     cy.url().should('include', '/ke-sach')
     cy.contains('.series-name a', 'Người Giấy').should('not.exist');
 })
+
+it("6 Hien thi canh bao khi theo doi truyen ma chua dang nhap", () => {
+
+  cy.visit("https://docln.sbs");
+
+  cy.wait(2000)
+  cy.get('#sidenav-icon').click()
+  cy.contains('a.nav-menu_item', 'Sáng tác').click();
+  cy.get('.popular-thumb-item a')
+  .then($items => {
+    const randomIndex = Math.floor(Math.random() * $items.length);
+    cy.wrap($items[randomIndex]).click({ force: true });
+  });
+   //chỉ định truyện
+    // cy.contains('.popular-thumb-item a', 'ten truyen').click({ force: true });
+
+  cy.get('#collect').click();
+
+  cy.on("window:alert", (message) => {
+    expect(message).to.equal("Cần đăng nhập để thực hiện!");
+  }); 
+});
